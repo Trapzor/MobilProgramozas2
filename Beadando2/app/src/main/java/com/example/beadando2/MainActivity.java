8161af5a15d987ded64175fa0ad984b8a6181edc
Trapzor <fejesbencegr@gmail.com>
2022-05-17 15:51:26 +0200

    Fix SensorActivity

--- Beadando2/app/src/main/java/com/example/beadando2/MainActivity.java
+++ Beadando2/app/src/main/java/com/example/beadando2/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.beadando2;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Lifecycle;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button sensorButton;
+
     MyLifeCycleObserver myLifeCycleObserver;
     Lifecycle lifecycle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,16 @@ public class MainActivity extends AppCompatActivity {
 
         lifecycle = getLifecycle();
         lifecycle.addObserver(myLifeCycleObserver);
+
+        sensorButton = findViewById(R.id.buttonToSensor);
+
+        sensorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent toSensor = new Intent(MainActivity.this, SensorActivity.class);
+                startActivity(toSensor);
+            }
+        });
     }
 
 
--- Beadando2/app/src/main/java/com/example/beadando2/SensorActivity.java
+++ Beadando2/app/src/main/java/com/example/beadando2/SensorActivity.java
@@ -2,13 +2,77 @@ package com.example.beadando2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class SensorActivity extends AppCompatActivity {
+import java.util.List;
+
+public class SensorActivity extends AppCompatActivity implements SensorEventListener {
+
+    SensorManager sensorManager;
+    List<Sensor> sensorsList;
+    Sensor accelerometer;
+    TextView sensorText, sensorsText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensor);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null){
+            accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        }else{
+            Toast.makeText(this, "No such sensor", Toast.LENGTH_SHORT).show();
+        }
+
+        sensorText = findViewById(R.id.sensorTextView);
+
+        sensorsText = findViewById(R.id.sensorsTextView);
+        sensorsList = sensorManager.getSensorList(Sensor.TYPE_ALL);
+        for(Sensor s : sensorsList){
+            sensorsText.append(
+                    "Name: " + s.getName() + "\n"
+                    + "Version: " + s.getVersion() + "\n"
+                    + "Vendor: " + s.getVendor() + "\n"
+                    + "Type: " + s.getType() + "\n\n");
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        String values = "Values: ";
+        for(float f : sensorEvent.values){
+            values += " ";
+            values += String.valueOf(f);
+        }
+        sensorText.setText(values);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(
+                this,
+                accelerometer,
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
     }
 }
